b35c8a5f9ad7158e7012b4b8720f3c9bc433f82d
dpmurphy11 <dpmurphy_onsite@hotmail.com>
2022-01-20 16:42:50 -0500

    js function definitions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var gameDiv = document.querySelector('#game');
-var scoresDiv = document.querySelector('#high-scores');
+var highScoresDiv = document.querySelector('#high-scores');
+var saveScoresDiv = document.querySelector('#save-scores');
+var startDiv = document.querySelector('#start');
 var scoresSpan = document.querySelector('#scores');
 var timerSpan = document.querySelector('#timer');
 var titleH1 = document.querySelector('#title');
@@ -10,7 +12,7 @@ var saveBtn = document.querySelector('#btn-save');
 var backBtn = document.querySelector('#btn-back');
 var clearBtn = document.querySelector('#btn-clear');
 var scoreH2 = document.querySelector('#final-score');
-var nameInp = document.querySelector('#name');
+var nameInput = document.querySelector('#name');
 
 var counter = 60;
 
@@ -18,35 +20,55 @@ var highScores = JSON.parse(localStorage.getItem('scores'));
 if (highScores === null) {
      highScores = [];
 }
-// highScores = [];
-console.log(highScores);
-// scoresDiv.setAttribute('style', 'visibility: hidden');
-// gameDiv.setAttribute('style', 'visibility: visible');
+// console.log(highScores);
 
 // question / answer data in nested arrays
-var arr = [];
-arr[0] = ['Quest1',['answer1', 'answer2', 'answer3', 'answer4'], 1, 10];
-arr[1] = ['Quest2',['answer1', 'answer2', 'answer3', 'answer4'], 2, 20];
-arr[2] = ['Quest3',['answer1', 'answer2', 'answer3', 'answer4'], 3, 10];
-arr[3] = ['Quest4',['answer1', 'answer2', 'answer3', 'answer4'], 4, 12];
-arr[4] = ['Quest5',['answer1', 'answer2', 'answer3', 'answer4'], 0, 25];
-console.log(arr);
+// Element 1: Question 
+// Element 2: Possible answers
+// Element 3: Correct answer
+// Element 4: Points for correct answer
+var arrQuestions = [];
+arrQuestions[0] = ['Quest1',['answer1', 'answer2', 'answer3', 'answer4'], 'answer3', 10];
+arrQuestions[1] = ['Quest2',['answer1', 'answer2', 'answer3', 'answer4'], 'answer1', 20];
+arrQuestions[2] = ['Quest3',['answer1', 'answer2', 'answer3', 'answer4'], 'answer4', 10];
+arrQuestions[3] = ['Quest4',['answer1', 'answer2', 'answer3', 'answer4'], 'answer2', 12];
+arrQuestions[4] = ['Quest5',['answer1', 'answer2', 'answer3', 'answer4'], 'answer1', 25];
+// console.log(arrQuestions);
 
 // functions
+function loadGame() {
+  startDiv.setAttribute('style', 'display: visible');
+  highScoresDiv.setAttribute('style', 'display: none');
+  saveScoresDiv.setAttribute('style', 'display: none');
+  gameDiv.setAttribute('style', 'display: none');
+
+  // shuffle array
+
+}
+
+loadGame();
+
 function gameStart(event) {
     event.preventDefault();
+    loadGame();
     // console.log(event);
-    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    // setInterval for every second
     var timeInterval = setInterval(function () {
       timerSpan.textContent = "Time: " + counter
-        if (counter > 0) {
-        // Decrement `counterby 1
+        if (counter > 0 && arrQuestions.length > 0) {
+          // get question
+
+          // render q & a
+          
+          // pop question
+
+          // 
         counter--;
       } else {
-        // Use `clearInterval()` to stop the timer
+        // stop counter
         clearInterval(timeInterval);
-        // Call the `displayMessage()` function
-        // displayMessage();
+
+        // 
       }
     }, 1000);
   
@@ -54,18 +76,36 @@ function gameStart(event) {
 
 function saveScore(event) {
     event.preventDefault();
-    highScores.push('abc', 123);
-    localStorage.setItem('scores', JSON.stringify(highScores));
+    startDiv.setAttribute('style', 'display: none');
+    highScoresDiv.setAttribute('style', 'display: none');
+    saveScoresDiv.setAttribute('style', 'display: visible');
+    gameDiv.setAttribute('style', 'display: none');
+  
+    // get initials
+
+    // save to local storage
+    localStorage.setItem('highScores', JSON.stringify(highScores));
 }
 
 function clearScores(event) {
     event.preventDefault();
-    localStorage.removeItem('scores');
+    localStorage.removeItem('highScores');
+
+    renderScores();
 }
 
 function renderScores(event) {
     event.preventDefault();
-    console.log(event);
+    startDiv.setAttribute('style', 'display: none');
+    highScoresDiv.setAttribute('style', 'display: visible');
+    saveScoresDiv.setAttribute('style', 'display: none');
+    gameDiv.setAttribute('style', 'display: none');
+  
+    // get scores
+
+    // order scores
+
+    // render scores
 }
 
 // event listeners
