1ff1a25c976e29bf400af21e4d1557a8eaebbac1
dpmurphy11 <dpmurphy_onsite@hotmail.com>
2022-01-20 03:42:23 -0500

    question / answers arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,15 @@ console.log(highScores);
 // scoresDiv.setAttribute('style', 'visibility: hidden');
 // gameDiv.setAttribute('style', 'visibility: visible');
 
+// question / answer data in nested arrays
+var arr = [];
+arr[0] = ['Quest1',['answer1', 'answer2', 'answer3', 'answer4'], 1, 10];
+arr[1] = ['Quest2',['answer1', 'answer2', 'answer3', 'answer4'], 2, 20];
+arr[2] = ['Quest3',['answer1', 'answer2', 'answer3', 'answer4'], 3, 10];
+arr[3] = ['Quest4',['answer1', 'answer2', 'answer3', 'answer4'], 4, 12];
+arr[4] = ['Quest5',['answer1', 'answer2', 'answer3', 'answer4'], 0, 25];
+console.log(arr);
+
 // functions
 function gameStart(event) {
     event.preventDefault();
